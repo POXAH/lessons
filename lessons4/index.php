21e5317721fde7627294698d803856fa9e05b416
Ruslan Azanov <crezi_23@mail.ru>
2019-04-05 21:53:14 +0500

    Update index.php

--- lessons4/index.php
+++ lessons4/index.php
@@ -4,12 +4,16 @@ class Product
     public $name;
     public $price;
     public $weight;
+    public $inclededVAT;
+    private $priceWithoutVAT;
 
-    public function __construct(string $name, float $price, float $weight)
+    public function __construct(string $name, float $price, float $weight, int $vat = 20, bool $inclededVAT = true)
     {
-        $this->price = $price;
         $this->weight = $weight;
         $this->name = $name;
+        $this->inclededVAT = $inclededVAT;
+        $this->priceWithoutVAT = ($inclededVAT) ? $price - round($price/(100+$vat) * $vat) : $price;
+        $this->price = ($inclededVAT) ? $price : round($price+$price*$vat/100);
     }
 
     public function printInfoVAT(){
@@ -18,13 +22,13 @@ class Product
 
     public function printInfoWithoutVAT(){
         $this->price -= round($this->price /120 * 20);
-        echo "Продукт с наименованием: $this->name. При весе $this->weight грамм имеет цену $this->price руб. без НДС<br><br>";
+        echo "Продукт с наименованием: $this->name. При весе $this->weight грамм имеет цену $this->priceWithoutVAT руб. без НДС<br><br>";
     }
 }
 
-$product1 = new Product('Хлеб', '25','350');
-$product2 = new Product('Молоко', '40','800');
-$product3 = new Product('Кола', '100','1000');
+$product1 = new Product('Хлеб', '25','350', 100, 0);
+$product2 = new Product('Молоко', '40','800', 18);
+$product3 = new Product('Кола', '100','1000', 20,0);
 $product1->printInfoVAT();
 $product1->printInfoWithoutVAT();
 $product2->printInfoVAT();
